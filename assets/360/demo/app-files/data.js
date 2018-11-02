c65f537345640fc874405d4d173dfcbb2989b14a
EricPoitras <EricPoitras@users.noreply.github.com>
2018-11-01 20:10:58 -0600

    Add files via upload

---
+++ assets/360/demo/app-files/data.js
@@ -0,0 +1,166 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1f6091f25c325c8359fdd25c50dbf0e2",
+      "name": "1F6091F25C325C8359FDD25C50DBF0E2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 672,
+      "initialViewParameters": {
+        "yaw": -3.123220636705975,
+        "pitch": 0.719737103603622,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4607031920565072,
+          "pitch": 0.7468603140537837,
+          "rotation": 0,
+          "target": "1-493e2f04ed00bd474af9e099391c5e3a"
+        },
+        {
+          "yaw": -2.255622083054,
+          "pitch": 0.8115562109106769,
+          "rotation": 0,
+          "target": "2-c11a2e70ef1ffd4a0b649b998befcba7"
+        },
+        {
+          "yaw": 3.0659748106119773,
+          "pitch": 1.4306835289082231,
+          "rotation": 0,
+          "target": "3-f5a713dbd8db866fac58b1bfe59e2925"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.100790533997948,
+          "pitch": 0.697307000895595,
+          "title": "Allosaurus",
+          "text": "Lots of teeth..."
+        }
+      ]
+    },
+    {
+      "id": "1-493e2f04ed00bd474af9e099391c5e3a",
+      "name": "493E2F04ED00BD474AF9E099391C5E3A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 672,
+      "initialViewParameters": {
+        "yaw": -3.1024148552923094,
+        "pitch": 0.6394487238534552,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.101404949691286,
+          "pitch": 0.6384388182524301,
+          "rotation": 0,
+          "target": "0-1f6091f25c325c8359fdd25c50dbf0e2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-c11a2e70ef1ffd4a0b649b998befcba7",
+      "name": "C11A2E70EF1FFD4A0B649B998BEFCBA7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 672,
+      "initialViewParameters": {
+        "yaw": -2.961716883741417,
+        "pitch": 0.9456046157070617,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9286679734397705,
+          "pitch": 0.5847901341320991,
+          "rotation": 0,
+          "target": "0-1f6091f25c325c8359fdd25c50dbf0e2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-f5a713dbd8db866fac58b1bfe59e2925",
+      "name": "F5A713DBD8DB866FAC58B1BFE59E2925",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 672,
+      "initialViewParameters": {
+        "yaw": -2.9137390745246243,
+        "pitch": 0.8234117638937875,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.008361099904951,
+          "pitch": 0.529578308857257,
+          "rotation": 0,
+          "target": "0-1f6091f25c325c8359fdd25c50dbf0e2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
